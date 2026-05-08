5ef125f102df6f7f96f172eca7d8504c0c702cb4
zainab <zainab.javaid2025@gmail.com>
2026-05-08 23:48:51 +0500

    NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-              <Link
+              <NavLink
                 className="flex flex-shrink-0 items-center mr-4"
                 to="/"
               >
@@ -19,27 +19,27 @@ const Navbar = () => {
                 <span className="hidden md:block text-white text-2xl font-bold ml-2">
                   React Jobs
                 </span>
-              </Link>
+              </NavLink>
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
-                  <Link
+                  <NavLink
                     to="/"
                     className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Home
-                  </Link>
-                  <Link
+                  </NavLink>
+                  <NavLink
                     to="/jobs"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Jobs
-                  </Link>
-                  <Link
+                  </NavLink>
+                  <NavLink
                     to="/add-job"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Add Job
-                  </Link>
+                  </NavLink>
                 </div>
               </div>
             </div>
