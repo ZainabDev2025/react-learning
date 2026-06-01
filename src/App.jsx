61b489a43a387fdd632cbfcc00732e3bb7393da6
zainab <zainab.javaid2025@gmail.com>
2026-06-01 19:03:30 +0500

    POST method

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,36 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import EachJobPage from "./pages/EachJobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EachJobPage, { jobLoader } from "./pages/EachJobPage";
+
+const App = () => {
+
+  const addJob = async (newJob)=>{
+  const res = await fetch ('/api/jobs',{
+    method :'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(newJob)
+
+  });
+  return;
+  
+} 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<EachJobPage/>} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} /> 
+ <Route path='/jobs/:id' element={<EachJobPage/>} loader ={jobLoader}/>
 
       <Route path='*' element={<NotFoundPage/>} />
 
     </Route>,
   ),
 );
-const App = () => {
   return <RouterProvider router={router} />;
 };
 
--- src/pages/EachJobPage.jsx
+++ src/pages/EachJobPage.jsx
@@ -1,39 +1,103 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import Spinner from '../components/Spinner'
-
+import React from "react";
+import { useParams, useLoaderData } from "react-router-dom";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const EachJobPage = () => {
-    const {id} =useParams ();
-  const [job, setJob] =useState(null)
-  const [loading,setLoading] = useState(true)
-useEffect(()=>{
-const fetchJob = async() =>{
-    try {
-//       const apiUrl = isHome 
-//   ? '/api/jobs?_page=1&_per_page=3' 
-//   : '/api/jobs';
+  const { id } = useParams();
+  const job = useLoaderData();
+  return (
+    <>
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            to="/jobs"
+            className="text-indigo-500 hover:text-indigo-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
+          </Link>
+        </div>
+      </section>
 
-        const res = await fetch(`/api/jobs/${id}`);  // ✅ already correct
-const data = await res.json();               // ✅ already correct
-setJob(data);                                // ✅ just this
-        
-setJobs(isHome ? data.data : data);
-      } catch (error) {
-        console.log("Error fetching data", error);
-      } finally {
-        setLoading(false);
-      }
+      <section className="bg-indigo-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <main>
+              <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+                <div className="text-gray-500 mb-4">{job.type}</div>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
+                <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                  <FaMapMarker className="text-orange-700 mr-1" />
+                  <p className="text-orange-700">{job.location}</p>
+                </div>
+              </div>
 
-}
-fetchJob();
-},[])
-  return loading ? <Spinner/> :(
-    <h1>
-       {job.title}
-    </h1>
-  )
-}
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-indigo-800 text-lg font-bold mb-6">
+                  {job.description}
+                </h3>
 
-export default EachJobPage
+                <p className="mb-4">
+                  We are seeking a talented Front-End Developer to join our team
+                  in Boston, MA. The ideal candidate will have strong skills in
+                  HTML, CSS, and JavaScript, with experience working with modern
+                  JavaScript frameworks such as React or Angular.
+                </p>
+
+                <h3 className="text-indigo-800 text-lg font-bold mb-2">
+                  Salary
+                </h3>
+
+                <p className="mb-4">{job.salary}</p>
+              </div>
+            </main>
+
+            <aside>
+              <div className="bg-white p-6 rounded-lg shadow-md">
+                <h3 className="text-xl font-bold mb-6">Company Info</h3>
+
+                <h2 className="text-2xl">{job.company.name}</h2>
+
+                <p className="my-2">{job.company.description}</p>
+
+                <hr className="my-4" />
+
+                <h3 className="text-xl">Contact Email:</h3>
+
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactEmail}
+                </p>
+
+                <h3 className="text-xl">Contact Phone:</h3>
+
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
+              </div>
+
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
+                <Link
+                  to= {`jobs/edit/${job.id}`}
+                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Edit Job
+                </Link>
+                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                  Delete Job
+                </button>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+export { EachJobPage as default, jobLoader };
